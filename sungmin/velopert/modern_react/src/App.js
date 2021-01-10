1720b3c9c1b39bc76eeb2ab90404c32208394350
pistosmin <tajiklee@naver.com>
2021-01-10 23:50:03 +0900

    project files moved to lecture folder

---
+++ sungmin/velopert/modern_react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Hello from './Hello';
+import Wrapper from './Wrapper';
+
+
+function App() {
+  return (
+    <Wrapper>
+      {/* <Hello name="react" color="red" isSpecial={true}/> */}
+      <Hello name="react" color="red" isSpecial />
+      <Hello color="pink" />
+    </Wrapper>
+  )
+}
+
+export default App;
