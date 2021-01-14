04dc50512017e15306fcf89b90f2055ea92172f9
youngvita <youngvita@gmail.com>
2021-01-14 19:05:43 +0900

    실습
    1-15까지 실습내용

---
+++ youngvita/velopert/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './1_5App';
+//import App from './1_6App';
+//import App from './1_7App';
+//import App from './1_8App';
+//import App from './1_9App';
+//import App from './1_10App';
+//import App from './1_11App';
+//import App from './1_12App';
+//import App from './1_13App';
+//import App from './1_14App';
+import App from './1_15App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
