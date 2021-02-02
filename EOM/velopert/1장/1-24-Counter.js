bdef03c32469773f2cc67a45cc49dce64d04e755
Eomok <umok98@naver.com>
2021-02-02 21:21:01 +0900

    1-24

---
+++ EOM/velopert/1장/1-24-Counter.js
@@ -0,0 +1,48 @@
+import React , {Component} from 'react'; 
+
+class Counter extends Component{
+  state = {
+    counter: 0,
+    fixed: 1
+  };
+
+  handleIncrease=()=>{
+    this.setState({
+      counter: this.state.counter + 1
+    });
+    
+    this.setState({
+      counter: this.state.counter + 1
+    });
+    this.setState(state => ({
+      counter: state.counter + 1
+    }));
+    this.setState(state => ({
+      counter: state.counter + 1
+    }));
+  };
+  handleDecrease=()=>{
+    this.setState({
+      counter: this.state.counter - 1
+      },
+      ()=>{
+        console.log(this.state.counter);
+      }
+    );
+  };
+  
+  
+  render(){ 
+  return(
+    <div> 
+      <h1>{this.state.counter}</h1>
+      
+      <button onClick={this.handleIncrease}>+1</button>
+      <button onClick={this.handleDecrease}>-1</button>
+      <p>고정된값 : {this.state.fixed}</p>
+    </div>
+   );
+  }
+}
+ 
+export default Counter;
